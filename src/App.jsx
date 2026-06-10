8ee650e9f0cf277fddc431fe0e5ebfdcd8277f67
swiftchai <lsing98@wgu.edu>
2026-06-10 15:02:22 -0700

    Added counter to samosa image

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import './App.css';
 import { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  function updateCount() {
+    setCount(count + multiplier);
+  }
 
   return (
     <div className="App">
-      <div className = "Header">
+      <div className="Header">
         <h1>Samosa Selector</h1>
-        <h2>Count:</h2>
-        <img className = "samosa" src="src/assets/img/Samosa.png" alt="samosa"/>
+        <h2>Count: {count}</h2>
+        <img className="samosa" src="src/assets/img/samosa.png" onClick={updateCount} alt="samosa"/>
       </div>
 
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
